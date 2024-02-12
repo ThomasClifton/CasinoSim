9d59a584af83049d3379ac4bcd2f896784abfe71
ThomasClifton <tsmithclifton@gmail.com>
2024-02-12 15:51:28 -0500

    Updated files to typescript

---
+++ src/containers/BlackjackGameContainer.tsx
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+
+const Blackjack = () => {
+    return (
+        <div>
+            <Link to="/">
+                <button>Back to home</button>
+            </Link>
+
+            <h2>Blackjack Game Page</h2>
+            {/* Add Blackjack game content */}
+        </div>
+    );
+};
+
+export default Blackjack;
---
+++ src/containers/HeaderContainer.tsx
@@ -0,0 +1,16 @@
+import './Header.css';
+
+const Header = ({ title = "Casino Sim", playerBalance = 1000}) => {
+    return (
+        <header className="app-header">
+            <div>
+                <h1>{title}</h1>
+            </div>
+            <div>
+                <h1>Player's Balance: ${playerBalance}</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
