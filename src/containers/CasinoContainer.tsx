ec1145db3c96e3657a4ad17557c4bf1eac1d032e
ThomasClifton <tsmithclifton@gmail.com>
2024-02-20 18:24:27 -0500

    Trying to get a background just for landing page

--- src/containers/CasinoContainer.tsx
+++ src/containers/CasinoContainer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import '../styles/_home.css';
+import '../styles/main.css';
 //import '../styles/_casino.css';
 
 const Casino = () => {
--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -4,11 +4,6 @@ import '../styles/_home.css';
 const Home = () => {
     return (
         <div className="home-container">
-            <h2>Welcome to Casino Sim!</h2>
-            <p>
-                Message about game
-            </p>
-
             <div className="home-buttons">
                 <Link to="/casino">
                     <button>Play</button>
