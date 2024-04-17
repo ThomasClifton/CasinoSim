17fb17193cb387d17136fb7a4cd41034afaa785f
ChamplainWilliam <william.chen@mymail.champlain.edu>
2024-04-17 17:16:11 -0400

    bomb game grid, doesn't render

--- src/containers/BombGameContainer.tsx
+++ src/containers/BombGameContainer.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "../components/header.tsx";
 import { useBalanceStore } from '../store/store';
 
@@ -5,11 +6,26 @@ import { useBalanceStore } from '../store/store';
 const Bomb = () => {
     const balance = useBalanceStore((state) => state.balance);
 
+    const [board, setBoard] = useState<boolean[][]>(Array(5).fill(Array(5).fill(false)));
+
+    const handleCellClick = (row: number, column: number) => {
+        console.log("Clicked on (${row}, ${column})");
+    }
+    
+
     return (
         <><Header balance={balance}/>
             <div>
                 <h2>Bomb Game Page</h2>
-                
+                <div className="bombContainer">
+                    {board.map((row, rowIndex) => (
+                        <div key={rowIndex} className="row">
+                          {row.map((cell, colIndex) => (
+                            <div key={colIndex} className={`cell${cell ? 'revealed' : ''}`} onClick={() => handleCellClick(rowIndex, colIndex)} />
+                          ))}
+                        </div>
+                      ))}
+                </div>
             </div>
         </>
     );
